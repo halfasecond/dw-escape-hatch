9d9094eb14d33d108661637405e2d161b3f62e9a
Jack Pickett <jack@half-a-second.com>
2024-11-13 13:31:20 +0000

    adds console.log to BASE_URL. (rogue redirect...)

--- src/components/App/AppView.tsx
+++ src/components/App/AppView.tsx
@@ -23,7 +23,8 @@ const core: Contract<AbiFragment[]> = getContract(Contracts.Core.abi, Contracts.
 const sale: Contract<AbiFragment[]> = getContract(Contracts.Sale.abi, Contracts.Sale.addr)
 const sire: Contract<AbiFragment[]> = getContract(Contracts.Sire.abi, Contracts.Sire.addr)
 
-const BASE_URL = import.meta.env.BASE_URL ? import.meta.env.BASE_URL : '/dw-escape-hatch'
+const BASE_URL = import.meta.env.BASE_URL ? import.meta.env.BASE_URL : '/dw-escape-hatch/'
+console.log("base", BASE_URL)
 
 const AppView: React.FC<AuthProps> = ({ handleSignIn, handleSignOut, loggedIn: walletAddress, isDapper }) => {
     const [contract, setContract] = useState<Contract<AbiFragment[]> | undefined>(undefined)
